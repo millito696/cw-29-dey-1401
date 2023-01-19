52adabe74051f7a5927aa38a4fa09d9012ccd5e5
millito696 <miladcoldblooded@gmail.com>
2023-01-19 19:36:38 +0330

    add logic and functionality

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import Button from './components/Buttons/Buttons';
+import { useEffect, useState } from 'react';
+
+let gameArray = [0, 0, 0, 0, 0, 0, 0, 0, 0];
 function App() {
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {gameArray.map((el, index) => <Button key={index + 1} id={index + 1} />)}
+      {/* {console.log(i)} */}
     </div>
   );
 }
